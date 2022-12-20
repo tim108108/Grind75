3eba70a3cc99a2990971307105e29e342b89dd6f
tim108108 <tim108108@gmail.com>
2022-12-20 16:13:56 +0800

    1613

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 class Solution(object):
-    def insert(self, intervals, newInterval):
-        ans = []
-        for interval in intervals:
-            if newInterval[0] > interval[1]: # head
-                ans.append(interval)
-            elif newInterval[1] < interval[0]: #tail
-                ans.append(newInterval)
-                newInterval = interval
-            else: # merge
-                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
-            print(newInterval)
-        ans.append(newInterval) # last
-        return ans
-intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-newInterval = [4,8]
-Solution().insert(intervals, newInterval)
+    def threeSum(self, nums):
+        nums.sort()
+        tmp=[]
+        for i in range(len(nums)-1):
+            j=i+1
+            k=len(nums)-1
+            if i>0 & nums[i]==nums[i-1]:
+                continue
+            while k>j:
+                if nums[i]+nums[j]+nums[k] > 0 :
+                    k-=1
+                elif nums[i]+nums[j]+nums[k] < 0 :
+                    j+=1
+                else :
+                    tmp.append([nums[i],nums[j],nums[k]])
+                    k-=1
+                    j+=1
+            print(tmp)
+        return tmp
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+n=[-1,0,1,2,-1,-4]
+n1=[0,0,0]
+Solution().threeSum(n)
