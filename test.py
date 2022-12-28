407f28d50c503e9abc1d360d8bcdb9ba13e532e8
tim108108 <tim108108@gmail.com>
2022-12-28 17:21:44 +0800

    1721

--- test.py
+++ test.py
@@ -1,27 +1,17 @@
 class Solution(object):
-    def threeSum(self, nums):
-        nums.sort()
-        tmp=[]
-        for i in range(len(nums)-1):
-            j=i+1
-            k=len(nums)-1
-            if i>0 & nums[i]==nums[i-1]:
-                continue
-            while k>j:
-                if nums[i]+nums[j]+nums[k] > 0 :
-                    k-=1
-                elif nums[i]+nums[j]+nums[k] < 0 :
-                    j+=1
-                else :
-                    tmp.append([nums[i],nums[j],nums[k]])
-                    k-=1
-                    j+=1
-            print(tmp)
-        return tmp
+    def merge(self, intervals):
+        if len(intervals)==0:
+            return []
+        sort_intervals = sorted(intervals, key=lambda x:x[0])
+        res=[sort_intervals[0]]
+        for interval in sort_intervals[1:]:
+            if interval[0]<=res[-1][1]:
+                res[-1][1]=max(interval[1],res[-1][1])
+            res.append(intervals)
+        return res
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-n=[-1,0,1,2,-1,-4]
-n1=[0,0,0]
-Solution().threeSum(n)
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+Solution().merge(intervals)
