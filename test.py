8366459d20fdd93db36463573d7c50e22bd3c1a7
tim108108 <tim108108@gmail.com>
2022-12-29 14:58:22 +0800

    1458

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 class Solution(object):
-    def sortColors(self, nums):
-        head = 0
-        tail = len(nums)-1
-        i = 0
-        while i <= tail:
-            if nums[i] ==0:
-                nums[head], nums[i] = nums[i], nums[head]
-                head+=1
-                i+=1
-            elif nums[i]==1:
-                i+=1
-            elif nums[i]==2:
-                nums[tail], nums[i]=nums[i], nums[tail]
-                tail-=1
-        return nums
-nums = [2,0,2,1,1,0]
-Solution().sortColors(nums)
+    def maxArea(self, height):
+        res=0
+        for i in range(len(height)):
+            for j in range(i+1,len(height)):
+                print(height[i],height[j])
+                k = (j-i)*min(height[i],height[j])
+                if k>res:
+                    res=k
+        print(res)
+        return res
+height = [1,1]
+Solution().maxArea(height)
